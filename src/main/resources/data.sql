5f97f65fc1b9079190ab3cbfa4b5f883b45dd92d
luketeosmu <luke.teo.2020@sis.smu.edu.sg>
2022-04-01 17:38:20 +0800

    added data to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,25 @@
 insert into user(id, authorities , password , username) values (1 , "ROLE_ADMIN", "Tester123", "luketeojianan");
 insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
-(1,'Marigold',2.2,2.2,2.2,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13079298_LXL1_20210610.jpg?w=1200&q=70','Mixed Berries Yogurt',2.2,2.2,2.2,2.2,'100g');
+(1,'Marigold',117,2.0,0.7,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13079298_LXL1_20210610.jpg?w=1200&q=70','Mixed Berries Yogurt',2.20,5.5,72,19.0,'130g');
 insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
-(2,'Skippy',2.2,2.2,2.2,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/47440_XL1_20210827.jpg?w=1200&q=70','Creamy Peanut Butter',2.2,2.2,2.2,2.2,'100g');
+(2,'Emmi Swiss',95,1.5,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/11216229_XL1.jpg?w=1200&q=70','Low Fat Strawberry Yogurt',1.1,4.0,4.0,15.0,'100g');
 insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
-(4,'Marigold',3.3,3.3,3.3,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13079298_LXL1_20210610.jpg?w=1200&q=70','Strawberry Yogurt',3.3,3.3,3.3,3.3,'100g');
+(3,'Marigold',90,1.9,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13025593_XL1_20210927.jpg?w=1200&q=70','Low Fat Yogurt- Natural',2.20,5.0,70,0.0,'130g');
 insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
-(5,'Marigold',1.1,1.1,1.1,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13079298_LXL1_20210610.jpg?w=1200&q=70','Peach Yogurt',1.1,1.1,1.1,1.1,'100g');
+(4,'Farmers Union',529,9.7,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/155863_LXL1.jpg?w=1200&q=70','Greek Style Yogurt- Natural',8.65,4.8,57,5.2,'100g');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(5,'Dairy Farmers',218,12.0,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13051600_XL1_20210111.jpg?w=1200&q=70','Thick & Creamy Yogurt - Classic Vanilla',2.75,5.3,69,22.2,'150g');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(6,'Farmers Union',529,9.7,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/155863_LXL1.jpg?w=1200&q=70','Greek Style Yogurt- Natural',8.65,4.8,57,5.2,'100g');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(7,'Fairprice',575,16.7,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13185255_XL1_20201023.jpg?w=1200&q=70','Peanut Butter- Creamy',3.00,7.0,96,4.4,'500g');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(8,'Adams',0,16.0,2.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/10070539_XL1_20220105.jpg?w=1200&q=70','100% Natural Peanut Butter- Crunchy',5.50,8.0,90,1.0,'32g');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(9,'Jif',190,16.0,2.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13049992_LXL1.jpg?w=1200&q=70','Peanut Butter Spread- Omega3',5.35,7.0,135,3.0,'33g');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(10,'Mayver\'s',486,9.8,1.6,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13075918_XL1_20210816.jpg?w=1200&q=70','All Natural Spread- Peanut Butter (Smooth)',6.95,4.8,51,0.8,'20g');
+
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(11,'Skippy',2.2,2.2,2.2,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/47440_XL1_20210827.jpg?w=1200&q=70','Creamy Peanut Butter',2.2,2.2,2.2,2.2,'100g');
 
