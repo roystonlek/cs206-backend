d94259adf3e75c4242f6499d515f6be86c4e673c
luketeosmu <luke@DESKTOP-57SUVC1.localdomain>
2022-04-03 21:21:54 +0800

    added milk to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,7 +12,7 @@ insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodiu
 insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
 (6,'Pauls',57,1.6,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13000991_XL1.jpg?w=1200&q=70','Natural Set Yogurt',9.45,4.1,60,0.0,'100g');
 insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
-(7,'Fairprice',575,16.7,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13185255_XL1_20201023.jpg?w=1200&q=70','Peanut Butter- Creamy',3.00,7.0,96,4.4,'500g');
+(7,'Fairprice',575,16.7,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13185255_XL1_20201023.jpg?w=1200&q=70','Peanut Butter- Creamy',3.00,7.0,96,4.4,'32g');
 insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
 (8,'Adams',0,16.0,2.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/10070539_XL1_20220105.jpg?w=1200&q=70','100% Natural Peanut Butter- Crunchy',5.50,8.0,90,1.0,'32g');
 insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
@@ -23,4 +23,21 @@ insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodiu
 (11,'Skippy',190,16,2.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/47440_XL1_20210827.jpg?w=1200&q=70','Creamy Peanut Butter',6.15,7.0,150.0,3.0,'32g');
 insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
 (12,'Smucker\'s',240,13.0,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/77265_XL1_20210107.jpg?w=1200&q=70','Goober Grape - Peanut Butter & Grape Jelly Stripes',5.55,7.0,140,3.0,'53g');
-
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(13,'Fairprice',115,3.3,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/12408855_LXL1_20201130.jpg?w=1200&q=70','UHT Milk - Low Fat',1.75,8.5,100,0.0,'250ml');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(14,'Dutch Lady',62,3.3,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/11570930_XL1_20210118.jpg?w=1024&q=70','UHT Milk - Full Cream(Plain)',2.50,3.2,0,0.0,'100ml');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(15,'Farmhouse',46,1.4,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13066631_XL1.jpg?w=1024&q=70','UHT Milk - Low Fat',2.10,3.3,0,5.1,'100g');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(16,'Harvey Fresh',375,0.5,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/10905401_XL1_20220307.jpg?w=1024&q=70','UHT Milk - Skim',2.30,7.8,90,11.8,'250ml');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(17,'F&N Magnolia',33,0.0,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/260050_XL1.jpg?w=1024&q=70','UHT Milk - Super Slim (Skim)',2.50,3.4,34,4.6,'100ml');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(18,'Cowhead',659,8.5,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/299091_XL1_20210430.jpg?w=1024&q=70','UHT Milk - Pure Milk',2.10,7.8,137.5,12.5,'250ml');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(19,'Meiji',146,9.2,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13099521_XL1.jpg?w=1200&q=70','Pasteurized Fresh Milk - Deluxe',3.25,6.8,84,9.2,'200ml');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(20,'F&N Magnolia',180,10.0,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/13022014_XL1_20211229.jpg?w=1200&q=70','Fresh Milk',3.50,10.0,0.0,10.8,'250ml');
+insert into product(id,brand,calories,fat,fibre,img_url,name,price,protein,sodium,sugar,serving_size) values
+(21,'Marigold',100,2.0,0.0,'https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/131467_LXL1_20210903.jpg?w=1200&q=70','HL Milk - Plain',3.20,10.0,152,2.4,'200ml');
